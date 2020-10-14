70586af92fecd0c0b0d4ea846d255d5a57b29175
ardopramudya <Ardoananta1@gmail.com>
2020-10-14 17:38:09 +0700

    init project

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('user-profile', 'AuthController@userProfile');
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -20,11 +32,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource("books","Bookcontroller");
 Route::get('/author', 'AuthorController@index');
-
-
 Route::post('/author/create', 'AuthorController@store');
-
-
 Route::post('/author/update/{id}', 'AuthorController@update');
-
 Route::delete('/author/delete/{id}', 'AuthorController@destroy');
